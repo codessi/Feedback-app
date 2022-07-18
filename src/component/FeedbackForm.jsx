9c3818931e4e90651b29bddaff6f72459bf05a97
codessi <johansuh@gmail.com>
2022-07-18 08:48:28 -0700

    rating jsx made

--- src/component/FeedbackForm.jsx
+++ src/component/FeedbackForm.jsx
@@ -1,12 +1,27 @@
 import React, { useState, useEffect } from 'react'
 import Card from './shared/Card'
 import Button from './shared/Button'
+import RatingSelect from './RatingSelect'
 
 const FeedbackForm = () => {
   const [text, setText] = useState("")
   const [btnDisabled, setBtnDisabled] = useState(true)
   const [message, setMessage] = useState("")
+  const [rating, setRating] = useState(10)
 
+
+// adding rating select
+// add state of rating   10 [v]
+  // crate RatingSelect.jsx and import[v]
+  // put right under the input[v]
+  // goto ratingselect.jsx [v]
+  // make state of select[v]
+  // add jsx
+  // ul class rating
+  // li input type radio id num1 name rating value 1 onChange{handleChange}
+  // checked={selected ===1 }
+  // label tag htmlFor="num1"
+  // make 10 of them 
   const handleChange = (event) => {
 
     if (text === "") {
@@ -29,7 +44,7 @@ const FeedbackForm = () => {
     <Card>
       <form>
         <h2>How would you rate your service with us?</h2>
-  
+        <RatingSelect />
         <div className="input-group">
           <input type="text" placeholder='Write a review' onChange={handleChange} value={text} />
           <Button type='submit' isDisabled={btnDisabled} >Send</Button>
