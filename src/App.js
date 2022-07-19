b8496174f248e5f755654f1fadf01b1326a5baaf
codessi <johansuh@gmail.com>
2022-07-19 08:13:38 -0700

    all finished

--- src/App.js
+++ src/App.js
@@ -1,40 +1,63 @@
-import React, {useState} from "react"
-import Header from "./component/Header"
+import React, { useState } from "react";
+import Header from "./component/Header";
 // import FeedbackItem from "./component/FeedbackItem"
-import FeedbackData from "./data/FeedbackData"
-import FeedbackList from "./component/FeedbackList"
-import FeedbackStats from "./component/FeedbackStats" 
-import FeedbackForm from "./component/FeedbackForm"
-
+import FeedbackData from "./data/FeedbackData";
+import FeedbackList from "./component/FeedbackList";
+import FeedbackStats from "./component/FeedbackStats";
+import FeedbackForm from "./component/FeedbackForm";
+import AboutPage from "./pages/AboutPage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AboutIconLink from "./component/AboutIconLink";
 
 const App = () => {
-  const [feedback, setFeedback] = useState(FeedbackData)
+ // nev link
+  // 
+  
+  const [feedback, setFeedback] = useState(FeedbackData);
 
   const deleteFeedback = (id) => {
-    if (window.confirm('Arey you sure you want to delete?')) {
-      setFeedback(feedback.filter((item) => { return item.id !== id }))
+    if (window.confirm("Arey you sure you want to delete?")) {
+      setFeedback(
+        feedback.filter((item) => {
+          return item.id !== id;
+        })
+      );
     }
-  }
+  };
 
   const addFeedBack = (newItem) => {
-    setFeedback(previous => {
-      return (
-        [newItem,...previous]
-    )})
-  }
+    setFeedback((previous) => {
+      return [newItem, ...previous];
+    });
+  };
   return (
-    <>  
-      <Header /> 
-      <div className="container">
-      <FeedbackForm feedback={feedback} addFeedBack={addFeedBack} />
-        <FeedbackStats feedback ={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-       
-        
-      </div>
+    <>
+      <Router>
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <>
+                  <FeedbackForm feedback={feedback} addFeedBack={addFeedBack} />
+                  <FeedbackStats feedback={feedback} />
+                  <FeedbackList
+                    feedback={feedback}
+                    handleDelete={deleteFeedback}
+                  />
+                </>
+              }
+            ></Route>
+
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+          <AboutIconLink />
+        </div>
+      </Router>
     </>
+  );
+};
 
-  )
-}
-
-export default App
+export default App;
