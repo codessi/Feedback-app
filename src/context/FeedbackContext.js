69f2c7900976436a0c851bd7195e3ec83212532e
codessi <johansuh@gmail.com>
2022-07-21 08:12:20 -0700

    comment fulled

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,27 +1,39 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import FeedbackData from "../data/FeedbackData";
 import { v4 as uuidv4 } from "uuid";
 
 const FeedbackContext = createContext({});
 // fetch the data & put in to feedback
-// emty out state
-// useEffect 
-// add function, []
-// add console.log(123)
-// fetch async function feedback  
+// emty out state [x]
+// useEffect  [v]
+// add function, [v]
+// add console.log(123) [v]
+// fetch async function feedback   [v]
 
-//await fetch("endpoint") add ?_sort=id&_order=desc assign to response
-// response.json assign to data
-// console.log data
-// run the feedback in useEffect
-// setState
-// add is loading for true
-// and setLoading to false end of useEffect
-// add isLoading to value to display spinner
+//await fetch("endpoint") add ?_sort=id&_order=desc assign to response []
+// response.json assign to data [v]
+// console.log data [v]
+// run the feedback in useEffect [v]
+// setState [v]
+// add is loading for true [v]
+// and setLoading to false end of useEffect [v]
+// add isLoading to value to display spinner [v]
 
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState(FeedbackData);
+  const [feedback, setFeedback] = useState([]);
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => { 
+    fetchFeedback()
+  }, [])
+
+  const fetchFeedback = async() => {
+    const response = await fetch("http://localhost:5000/feedback?_sort=id&_order=desc")
+    const data = await response.json()
+    setFeedback(data)
+    setLoading(false)
+  }
 
   const updateFeedback = (id, updItem) => {
     setFeedback(
